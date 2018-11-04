5dbe22fb342b59a2515a61fad357f2e5ef780071
Charalampos Fanoulis <38255093+cfanoulis@users.noreply.github.com>
2018-11-04 13:55:38 +0200

    updoot

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const q = [{
 i.prompt(q).then(ans => {
   if(isNaN(parseInt(ans.input))) throw "that aint an integer"
     for (let a of ans.input) {
-      console.log(ch.bgGreen(a));
+      console.log(a);
     }
     console.log(ch.bgBlue("Done"));
 });
