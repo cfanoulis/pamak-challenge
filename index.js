b6caf8c111d34c987219c5181174749d17d5bbef
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2018-11-04 11:46:51 +0000

    stoof
    Signed-off-by: Charalampos Fanoulis <charalampos.fanoulis@gmail.com>

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const i = require("inquirer");
 const q = [{
  type: 'input',
  name: 'input',
- message: chalk.blue("Enter a number")
+ message: ch.blue("Enter a number")
 }];
 
 i.prompt(q).then(ans => {
     for (let a of ans.input) {
-      console.log(chalk.bgGreen(a));
+      console.log(ch.bgGreen(a));
     }
-    console.log(chalk.bgWhite("Done"));
+    console.log(ch.bgWhite("Done"));
 });
